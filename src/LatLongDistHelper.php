f9891f1c28ed3ab22236cc8a716c81c38036482d
Dominik Schwind <dschwind@lostfocus.de>
2025-09-01 20:12:48 +0200

    feat(LatLongDIstHelper): Types

--- src/LatLongDistHelper.php
+++ src/LatLongDistHelper.php
@@ -1,21 +1,27 @@
 <?php
+declare(strict_types=1);
 
 namespace Famex\Helpers;
 
-class LatLongDistHelper {
-	public static function calculate($lat1, $lon1, $lat2, $lon2, $unit = "K"){
-		$theta = $lon1 - $lon2;
-		$dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
-		$dist = acos($dist);
-		$dist = rad2deg($dist);
-		$miles = $dist * 60 * 1.1515;
-		$unit = strtoupper($unit);
-		if ($unit == "K") {
-			return ($miles * 1.609344);
-		} else if ($unit == "N") {
-			return ($miles * 0.8684);
-		} else {
-			return $miles;
-		}
-	}
+class LatLongDistHelper
+{
+    public static function calculate(float $lat1, float $lon1, float $lat2, float $lon2, string $unit = "K"): float
+    {
+        $theta = $lon1 - $lon2;
+        $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+        $dist = acos($dist);
+        $dist = rad2deg($dist);
+        $miles = $dist * 60 * 1.1515;
+        $unit = strtoupper($unit);
+
+        if ($unit === "K") {
+            return ($miles * 1.609344);
+        }
+
+        if ($unit === "N") {
+            return ($miles * 0.8684);
+        }
+
+        return $miles;
+    }
 }
